a6ce9633acb3740add2eaae9246ed63c3fd9e69f
Jacob Sephton <redacted@spephton.net>
2023-05-12 01:12:03 +1000

    Stringify bits of a number in human-ish format

--- bitfuncs.c
+++ bitfuncs.c
@@ -7,6 +7,7 @@ int invert(int x, int p, int n);
 int bit_string(int x, char string[], int bounds);
 int mystrlen(char *string);
 void rev_str(char *dst, char *string);
+void bit_stringh(int x, char *bitstr, int bounds);
 
 int main(int argc, char *argv[]) {
 //	int i = atoi(argv[1]);
@@ -16,7 +17,6 @@ int main(int argc, char *argv[]) {
 	char string[32];
 //	int len = bit_string(i, string, 32);
 //	printf("%s has length %i\n", string, len);
-	printf("the length of %s is %i\n", "hello", mystrlen("hello"));
 
     char *normal = "a dog a panic in a pagoda";
 	char reversed[32];
@@ -24,6 +24,22 @@ int main(int argc, char *argv[]) {
 	printf("%s\n", normal);
 	printf("%s\n", reversed);
 	
+	int nums[] = {1, 2, 3, 4};
+	int nums_length = 4;
+
+	int i;
+	int bounds = 32;
+	char backwards_bitstring[bounds];
+	char corrected_bitstring[bounds];
+	for (i = 0; i < nums_length; i++) {
+		bit_string(nums[i], backwards_bitstring, bounds);
+		rev_str(corrected_bitstring, backwards_bitstring);
+		printf("%s\n", backwards_bitstring);
+		printf("%s\n", corrected_bitstring);
+		bit_stringh(nums[i], corrected_bitstring, bounds);
+		printf("%s\n", corrected_bitstring);
+	}
+
 	return 0;	
 }
 
@@ -51,7 +67,6 @@ int bit_string(int x, char string[], int bounds) {
 		x >>= 1;
 	}
 	string[j] = '\0';
-	printf("%s\n", string);
 	return j;
 }
 
@@ -62,7 +77,7 @@ void rev_str(char *dst, char *string) {
 	int len = mystrlen(string);
 	
 	int i;
-	for (i = 0; i <= len; i++) {
+	for (i = 0; i < len; i++) {
 		dst[i] = string[len - i - 1];
 	}
 	dst[i] = '\0';
@@ -72,4 +87,12 @@ int mystrlen(char *string) {
 	int i;
 	for (i = 0; string[i] != 0; i++) {}
 	return i;
+}
+
+void bit_stringh(int x, char *bitstr, int bounds) {
+	// the 'h' is alluding to e.g. ls -lh
+	// bit string returned in human readable order
+	char reversed[bounds];
+	bit_string(x, reversed, bounds);
+	rev_str(bitstr, reversed);
 }
